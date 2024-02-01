6da66cd3a5c50fb8a1e138d198bc7204e8e517d9
bsheth <bsheth@wisc.edu>
2024-02-01 01:14:33 -0600

    update

--- src/content-script.js
+++ src/content-script.js
@@ -1,2 +1,23 @@
-let cur = document.getElementById("hoverable-outlet-company-filter-value");
-console.log(cur);
+waitForAndGetElement("#hoverable-outlet-company-filter-value").then( (cur) => {
+	console.log(`cur: ${cur}`);
+});
+
+function waitForAndGetElement(selector) {
+	return new Promise( (resolve) => {
+		let ret = document.querySelector(selector);
+		if (ret) {
+			return resolve(ret);
+		}
+
+		const observer = new MutationObserver((mutations) => {
+			let ret = document.querySelector(selector);
+
+			if (ret) {
+				observer.disconnect();
+				return resolve(ret);
+			}
+		});
+
+		observer.observe(document.body, {subtree: true, childList: true});
+	});
+}
